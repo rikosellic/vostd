5252e081df80a9bff355cdf21130cb69cbb10985
Xungan2 <2100012996@stu.pku.edu.cn>
2025-04-16 00:27:48 +0000

    Merged
    Signed-off-by: Zhang Junyang <junyang@stu.pku.edu.cn>

--- lock-protocol/src/lib.rs
+++ lock-protocol/src/lib.rs
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> Temp
 // #![feature(generic_const_exprs)]
 #![allow(unused)]
 #![feature(slice_ptr_get)]
@@ -9,34 +5,20 @@
 #![feature(core_intrinsics)]
 #![allow(non_snake_case)]
 #![feature(sync_unsafe_cell)]
-<<<<<<< HEAD
 #![allow(type_alias_bounds)]
 
 pub mod mm;
 pub mod prelude;
 pub mod spec;
 // pub mod test;
-=======
-
-pub mod mm;
-pub mod spec;
-pub mod prelude;
-pub mod test;
->>>>>>> Temp
 pub mod task;
 pub mod x86_64;
 #[macro_use]
 pub mod helpers;
 pub mod sync;
 
-<<<<<<< HEAD
 pub mod exec;
 
 fn main() {
     exec::main_test();
 }
-=======
-fn main() {
-    spec::simple_page_table::main();
-}
->>>>>>> Temp
