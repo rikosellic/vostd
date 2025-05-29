e33588d9035b3f0eeb6b129843dddd7a21662b46
rikosellic <64517311+rikosellic@users.noreply.github.com>
2025-05-30 00:52:48 +0800

    rustc 1.85.1

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -216,7 +216,7 @@ struct BootstrapArgs {
     restart: bool,
 
     #[arg(long = "rust-version", help = "The rust version to use",
-        default_value = "1.82.0", action = ArgAction::Set)]
+        default_value = "1.85.1", action = ArgAction::Set)]
     rust_version: String,
 }
 
@@ -227,7 +227,7 @@ struct UpdateArgs {
     no_verus: bool,
 
     #[arg(long = "rust-version", help = "The rust version to use",
-        default_value = "1.82.0", action = ArgAction::Set)]
+        default_value = "1.85.1", action = ArgAction::Set)]
     rust_version: String,
 }
 
