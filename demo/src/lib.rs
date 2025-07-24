1af89a77efc4d5ccd5642856e099dd386d515bc5
SNoAnd <33380099+SNoAnd@users.noreply.github.com>
2025-07-24 01:52:07 -0700

    Add total

--- demo/src/lib.rs
+++ demo/src/lib.rs
@@ -1,13 +1,18 @@
 #![no_std]
 
+mod ptr_extra;
+use ptr_extra::*;
+
 pub struct DataCell<'a> {
     a: &'a mut u32,
     b: &'a mut u32,
+
+    total: u32,
 }
 
 impl DataCell<'_> {
     pub fn a_to_b(self, i: u32) {
-        *self.a -= i;
-        *self.b += i;
+        update_field!(self => a -= i, &mut);
+        update_field!(self => b += i, &mut);
     }
 }
