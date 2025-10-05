674c832197fb3f84e4030c3e75555b31b9d0fb5c
SNoAnd <33380099+SNoAnd@users.noreply.github.com>
2025-10-04 23:36:16 -0700

    Child `into_pte` spec

--- specs/ostd/src/lib.rs
+++ specs/ostd/src/lib.rs
@@ -1,5 +1,7 @@
 mod linked_list_specs;
 mod meta_specs;
+mod page_table_node_specs;
 
 pub use linked_list_specs::*;
 pub use meta_specs::*;
+pub use page_table_node_specs::*;
--- specs/ostd/src/page_table_node_specs.rs
+++ specs/ostd/src/page_table_node_specs.rs
@@ -1,6 +1,21 @@
 use vstd::prelude::*;
 
+use aster_common::prelude::*;
+use vstd_extra::ownership::*;
+
 verus! {
 
-    
+impl<C: PageTableConfig> Child<C> {
+
+    #[rustc_allow_incoherent_impl]
+    pub open spec fn into_pte_spec(self, slot_own : &MetaSlotOwner,
+                                        slot_perm : &vstd::simple_pptr::PointsTo<MetaSlot>) -> C::E {
+        match self {
+            Child::PageTable(node) => C::E::new_pt_spec(slot_perm.value().frame_paddr_spec(slot_own@)),
+            Child::Frame(paddr, level, prop) => C::E::new_page_spec(paddr, level, prop),
+            Child::None => C::E::new_absent_spec(),
+        }
+    }
+}
+
 }
