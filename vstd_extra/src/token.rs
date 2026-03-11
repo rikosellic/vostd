9b8d53d7c33cbd3ec323b6e63f6bac879b3eb876
rikosellic <64517311+rikosellic@users.noreply.github.com>
2026-03-11 11:51:01 +0800

    Update token library

--- vstd_extra/src/token.rs
+++ vstd_extra/src/token.rs
@@ -1,28 +1,43 @@
 use vstd::pcm::Loc;
 use vstd::prelude::*;
-use vstd::tokens::frac::{Frac,Empty};
+use vstd::tokens::frac::{Frac,Empty, FracGhost};
 use crate::sum::*;
 
 verus!{
 
-/// A struct that stores and dispatches `Frac<T>`.
-pub tracked struct FracStorage<T, const TOTAL: u64> {
-    pub tracked resource: Sum<Frac<T,TOTAL>, Empty<T,TOTAL>>
+/// A struct that stores and dispatches `FracGhost<T>`. 
+/// Unlike `FracGhost`, it provides an `empty` state.
+pub tracked struct FracGhostStorage<T, const TOTAL: u64> {
+    tracked r: Option<FracGhost<T,TOTAL>>,
+    ghost id: Loc,
 }
 
-impl <T, const TOTAL: u64> FracStorage<T, TOTAL> {    
-    pub open spec fn is_empty(self) -> bool {
-        self.resource is Right
+impl <T, const TOTAL: u64> FracGhostStorage<T, TOTAL> {
+    #[verifier::type_invariant]
+    spec fn type_inv(self) -> bool {
+        self.r is Some ==> self.id == self.r->Some_0.id()
     }
 
-    pub open spec fn not_empty(self) -> bool {
+    pub closed spec fn is_empty(self) -> bool {
+        self.r is None
+    }
+
+    pub closed spec fn not_empty(self) -> bool {
         !self.is_empty()
     }
 
-    pub open spec fn storage(self) -> Frac<T,TOTAL> 
+    /// Returns the `FracGhost<T,TOTAL>` stored in this `FracGhostStorage`.
+    pub closed spec fn storage(self) -> FracGhost<T,TOTAL> 
     recommends self.not_empty()
     {
-        self.resource->Left_0
+        self.r->Some_0
+    }
+
+    /// Returns the `T` stored in this `FracGhostStorage`.
+    pub closed spec fn view(self) -> T 
+    recommends self.not_empty()
+    {
+        self.storage().view()
     }
 
     pub open spec fn frac(self) -> int {
@@ -33,18 +48,26 @@ impl <T, const TOTAL: u64> FracStorage<T, TOTAL> {
         }   
     }
 
-    pub open spec fn id(self) -> Loc {
-        if self.is_empty() {
-            self.resource -> Right_0.id()
-        }
-        else {
-            self.storage().id()
-        }
+    pub closed spec fn id(self) -> Loc {
+        self.id
     }
-} 
 
-pub type TokenStorage<const TOTAL: u64> = FracStorage<(), TOTAL>;
-
-pub type SingleTokenStorage = TokenStorage<1>;
-
+    /*pub proof fn split_one(tracked &mut self) -> (tracked res: Frac<T,TOTAL>)
+    requires
+        old(self).not_empty(),
+    ensures
+        self.id() == old(self).id(),
+        self.frac() + 1 == old(self).frac(),
+        res.frac() == 1,
+        res.id() == self.id(),
+        res.resource() == old(self).resource(),
+        old(self).frac() > 1 ==> self.resource() == old(self).resource(),
+        old(self).frac() == 1 ==> self.is_empty(),
+    {
+        self.storage().bounded();
+        if self.frac() == 1 {
+            let r = self.
+        }
+    } */
+}
 }
